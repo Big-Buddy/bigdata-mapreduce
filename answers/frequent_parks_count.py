c4ac74841d435e4191764d8166ab057734f85c6a
Big-Buddy <a.pelletier0x7c9@gmail.com>
2018-01-29 17:24:28 -0500

    test frequent_parks_count

--- answers/frequent_parks_count.py
+++ answers/frequent_parks_count.py
@@ -28,10 +28,13 @@ for row in unique_parks_counts:
 	if not most_frequent:
 		most_frequent.append(row)
 	
-	for top_row in most_frequent:
+	if len(most_frequent) == 10:
+		for top_row in most_frequent:
 		if row[1] > top_row[1]:
 			del most_frequent[most_frequent.index(top_row)]
 			most_frequent.append(row)
+	else:
+		most_frequent.append(row)
 
 unique_parks_counts.sort(key = lambda tup: tup[1], reverse = True)
 
