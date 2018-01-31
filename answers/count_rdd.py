948ef4add93d4b0387dc5d3061ba20fa9ae390e1
Big-Buddy <a.pelletier0x7c9@gmail.com>
2018-01-30 21:40:51 -0500

    added count_rdd

---
+++ answers/count_rdd.py
@@ -0,0 +1,15 @@
+import sys
+import csv
+from pyspark import SparkContext, SparkConf
+
+file_name = sys.argv[1]
+
+conf = SparkConf().setAppName(count).setMaster(master)
+sc = SparkContext(conf=conf)
+
+rdd = sc.textFile(file_name)
+rdd = rdd.mapParitions(lambda x: csv.reader(x))
+header = rdd.first()
+rdd = rdd.filter(lambda x: x != header)
+
+print(rdd.count())
