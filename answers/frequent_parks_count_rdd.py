c5a18f45fe7605f2a0a376a2e67c707a0a624d8a
Big-Buddy <a.pelletier0x7c9@gmail.com>
2018-02-01 17:34:48 -0500

    added frequent rdd

---
+++ answers/frequent_parks_count_rdd.py
@@ -0,0 +1,21 @@
+import sys
+import csv
+from pyspark import SparkContext, SparkConf
+
+file_name = sys.argv[1]
+
+conf = SparkConf().setAppName("lab1").setMaster("local")
+sc = SparkContext(conf=conf)
+
+rdd = sc.textFile(file_name)
+rdd = rdd.mapPartitions(lambda x: csv.reader(x))
+header = rdd.first()
+rdd = rdd.filter(lambda x: x != header)
+
+rdd = rdd.filter(lambda x: x[6] != '')
+rdd = rdd.map(lambda x: (x[6], 1))
+rdd = rdd.reduceByKey(lambda a, b: a+b)
+rdd = rdd.sortBy(lambda x: x[1])
+
+for row in rdd.take(10):
+	print(row[0] + ": " + str(row[1]))
