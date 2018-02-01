5b80a0409ab4e233fb6401faf124d4227af1db32
Big-Buddy <a.pelletier0x7c9@gmail.com>
2018-02-01 17:24:26 -0500

    better solution for frequent parks counts

--- answers/frequent_parks_count.py
+++ answers/frequent_parks_count.py
@@ -4,39 +4,28 @@ from operator import itemgetter
 
 csv_name = sys.argv[1]
 with open(csv_name, encoding='utf-8') as f:
-	unique_parks_counts = []
+	most_frequent = []
 	reader = csv.reader(f)
 	next(reader)
 	for row in reader:
-		if not unique_parks_counts and row[6]:
-			unique_parks_counts.append((row[6], 1))
+		if not most_frequent and row[6]:
+			most_frequent.append((row[6], 1))
 
-		if row[6] and unique_parks_counts:		
+		if row[6] and most_frequent:		
 			flag = False		
 		
-			for unique_tuple in unique_parks_counts:
+			for unique_tuple in most_frequent:
 				if row[6] in unique_tuple:	
 					flag = True
-					ptr = unique_parks_counts.index(unique_tuple)			
+					ptr = most_frequent.index(unique_tuple)			
 					
 			if flag:
-				unique_parks_counts[ptr] = (unique_parks_counts[ptr][0], unique_parks_counts[ptr][1]+1)
+				most_frequent[ptr] = (most_frequent[ptr][0], most_frequent[ptr][1]+1)
 			else:
-				unique_parks_counts.append((row[6], 1))
-
-most_frequent = []
-for row in unique_parks_counts:
-	if not most_frequent:
-		most_frequent.append(row)
-	
-	if len(most_frequent) == 10:
-		if row[1] > min(most_frequent, key = itemgetter(1))[1]:
-			del most_frequent[most_frequent.index(min(most_frequent, key = itemgetter(1)))]
-			most_frequent.append(row)
-	else:
-		most_frequent.append(row)
+				most_frequent.append((row[6], 1))
 
 most_frequent.sort(key = itemgetter(1), reverse = True)
 
-for row in most_frequent:
-	print(row[0] + ": " + str(row[1]))
+
+for row in most_frequent[0:9]:
+	print(row[0] + ": " + str(row[1]))f
