8b5822c4981e494bf9a581f4e8f77efa3c38658e
Big-Buddy <a.pelletier0x7c9@gmail.com>
2018-02-01 20:56:44 -0500

    added uniq park counts

---
+++ answers/uniq_parks_counts_df.py
@@ -0,0 +1,20 @@
+import sys
+from pyspark.sql import SparkSession
+from pyspark.sql import Row
+
+file_name = sys.argv[1]
+
+spark = SparkSession.builder.master("local").appName("lab1").getOrCreate()
+
+df = spark.read.csv(file_name, header = True)
+
+df = df.filter(df['Nom_parc'] != '')
+
+df = df.withColumn('num_of_trees', 1)
+
+df = df.orderBy(['Nom_parc'], ascending = True)
+
+df = df.groupBy('Nom_parc')
+
+for row in df.collect():
+	print(row['Nom_parc'] + ": " + str(row['num_of_trees']))
