fb92e1b8a1af6d7db5a939c58bba518375c45987
Big-Buddy <a.pelletier0x7c9@gmail.com>
2018-02-01 21:49:42 -0500

    test intersection df

--- answers/intersection_df.py
+++ answers/intersection_df.py
@@ -9,15 +9,14 @@ spark = SparkSession.builder.master("local").appName("lab1").getOrCreate()
 
 df_1 = spark.read.csv(csv_1, header = True)
 df_1 = df_1.filter(df_1['Nom_parc'] != '')
-df_1 = df_1.orderBy(['Nom_parc'], ascending = True)
 df_1 = df_1.distinct()
 
 df_2 = spark.read.csv(csv_2, header = True)
 df_2 = df_2.filter(df_2['Nom_parc'] != '')
-df_2 = df_2.orderBy(['Nom_parc'], ascending = True)
 df_2 = df_2.distinct()
 
 df_intersect = df_1.intersect(df_2)
+df_intersect = df_intersect.orderBy(['Nom_parc'], ascending = True)
 
 for row in df_intersect.collect():
 	print(row['Nom_parc'])
