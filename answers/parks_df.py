93d321f12bcbb7c2b3e47495845d80459bf865a7
Big-Buddy <a.pelletier0x7c9@gmail.com>
2018-02-01 20:18:51 -0500

    added parks df

---
+++ answers/parks_df.py
@@ -0,0 +1,10 @@
+import sys
+from pyspark.sql import SparkSession
+
+file_name = sys.argv[1]
+
+spark = SparkSession.builder.master("local").appName("lab1").getOrCreate()
+
+df = spark.read.csv(file_name, header = True)
+
+print(df.filter(df['Nom_parc'] != '').count())
