85e1a259f470f97fee8dc66aeb26a0ba69d03d04
Big-Buddy <a.pelletier0x7c9@gmail.com>
2018-02-01 20:35:46 -0500

    test uniq parks df

--- answers/uniq_parks_df.py
+++ answers/uniq_parks_df.py
@@ -7,6 +7,6 @@ spark = SparkSession.builder.master("local").appName("lab1").getOrCreate()
 
 df = spark.read.csv(file_name, header = True)
 
-df = df.groupBy('Nom_parc').orderBy(['Nom_parc'], ascending = True)
+df = df.orderBy(['Nom_parc'], ascending = True)
 
 df.select('Nom_parc').show()
