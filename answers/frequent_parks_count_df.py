25599cc3f6e22e50811523f064c62029414259c6
Big-Buddy <a.pelletier0x7c9@gmail.com>
2018-02-01 21:36:51 -0500

    added freq parks count df

---
+++ answers/frequent_parks_count_df.py
@@ -0,0 +1,23 @@
+import sys
+from pyspark.sql import SparkSession
+from pyspark.sql import Row
+from pyspark.sql.functions import lit
+
+file_name = sys.argv[1]
+
+spark = SparkSession.builder.master("local").appName("lab1").getOrCreate()
+
+df = spark.read.csv(file_name, header = True)
+
+df = df.filter(df['Nom_parc'] != '')
+
+df = df.withColumn('num_of_trees', lit(1))
+
+df = df.orderBy(['Nom_parc'], ascending = True)
+
+df = df.groupBy('Nom_parc').agg({'num_of_trees': 'sum'})
+
+df = df.orderBy(['sum(num_of_trees)'], ascending = False)
+
+for row in df.take(10):
+	print(row['Nom_parc'] + ": " + str(row['sum(num_of_trees)']))
